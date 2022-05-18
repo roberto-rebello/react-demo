477c10c4662b4f1dee7efa587ef1f8f365016a7f
Roberto Rebello <roberto.rebello@gmail.com>
2022-05-17 23:00:05 -0300

    Adding state to Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,13 @@
+import {useState} from "react";
+
 function Message(props) {
+    const [message, setMessage] = useState("Welcome Visitor!");
+
     return (
-        <h1>Welcome Visitor!</h1>
+        <>
+        <h1>{message}</h1>
+        <button onClick={() => setMessage("Thank you for subscribing!")}>Subscribe!</button>
+        </>
 )
 }
 
